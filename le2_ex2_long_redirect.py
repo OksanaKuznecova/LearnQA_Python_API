320fa0b50bcf04ca2c91237341e8e70ec0795600
Oksana_Kuznetsova <oksana-1.kuznetsova@alcon.com>
2024-09-26 12:28:34 +0300

    Les2 Ex2: Long redirect

---
+++ le2_ex2_long_redirect.py
@@ -0,0 +1,29 @@
+import requests
+
+url = "https://playground.learnqa.ru/api/long_redirect"
+# I added these headers because without them the redirect is failing with 403 code when redirecting to
+# https://learnqa.ru/
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/129.0.0.0 Safari/537.36"}
+
+response = requests.get(url, allow_redirects=True, headers=headers)
+
+# Getting the response history
+response_history = response.history
+# Getting the response history length to identify the number of redirects
+response_history_length = len(response.history)
+print(f"The number of redirects is {response_history_length}")
+if response_history_length == 0:
+    print(f"looks like this endpoint does not have any redirects")
+else:
+    h = 0
+    while h < response_history_length:
+        # Checking each redirect
+        redirect_response = response_history[h]
+        # Printing each redirect URL and status code
+        print(redirect_response.url)
+        print(redirect_response.status_code)
+        h += 1
+    # the final URL and status code
+    print(f"The final URL is {response.url}")
+    print(f"The final URL is {response.status_code}")
